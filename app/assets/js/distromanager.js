79571cd7855144c93c0601b7f296dfb944c68327
sch8402 <82961569+sch8402@users.noreply.github.com>
2024-02-25 02:49:03 +0900

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+        const distroURL = 'http://52.79.237.43/msrgirin/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
